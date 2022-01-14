5d79019e489c1420452994cb95acd27bddf13c14
许涛 <https://github.com/liubenyin/liu-java.git>
2022-01-14 18:06:49 +0800

    diyici

---
+++ src/com/sict/test/lby.java
@@ -0,0 +1,9 @@
+package com.sict.test;
+
+public class lby {
+    public static void main(String[] args) {
+
+        System.out.println("wisihdysgusfdsd");
+        System.out.println("lallaa");
+    }
+}
